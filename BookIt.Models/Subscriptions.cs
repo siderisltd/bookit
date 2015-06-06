36c00b7978aed86e4dbaaa801a82cfff39429219
siderisltd <siderisltd@gmail.com>
2015-06-06 17:09:44 +0300

    Added Object models

---
+++ BookIt.Models/Subscriptions.cs
@@ -0,0 +1,17 @@
+﻿namespace BookIt.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Subscriptions
+    {
+        public DateTime SubscribedOn { get; set; }
+
+        public DateTime? SubscriptionEnd { get; set; }
+
+        public int BusinessID { get; set; }
+
+        public bool SubscribedForEmails { get; set; }
+    }
+}
---
+++ BookIt.Models/Unit.cs
@@ -0,0 +1,29 @@
+﻿namespace BookIt.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Unit
+    {
+        public int ID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public char Gender { get; set; }
+
+        public int Experience { get; set; }
+
+        public float Rating { get; set; }
+
+        public string Biography { get; set; }
+
+        public virtual ICollection<Service> ServicesProvided { get; set; }
+
+        public virtual ICollection<DateTime> Schedule { get; set; }
+    }
+}
