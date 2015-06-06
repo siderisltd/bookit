8561ca04246a2cceca3e1fa41e5c9341a75333d3
evlogihr <evlogihristov@yahoo.com>
2015-06-06 14:56:19 +0300

    Added ID to Service

--- BookIt.Models/Service.cs
+++ BookIt.Models/Service.cs
@@ -2,12 +2,14 @@
 {
     public class Service
     {
-        public int CategoryID { get; set; }
-
-        public virtual Category Category { get; set; }
+        public int ID { get; set; }
 
         public int Duration { get; set; }
 
         public decimal Price { get; set; }
+
+        public int CategoryID { get; set; }
+
+        public virtual Category Category { get; set; }
     }
 }
